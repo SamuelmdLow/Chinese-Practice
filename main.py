0dc0f49fa0244f7f5d72ff0b40d53e9b7fa1aaae
Samuel Low <s.low@share.epsb.ca>
2021-10-07 17:29:05 -0600

    Addded support for multiple databases!

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from random import shuffle
+from pathlib import Path
 
 def myFunc(e):
   return e.RankingValue
 
-
 class Word:
-    def __init__(self,Chinese, English):
+    def __init__(self,Chinese, PinYin, English):
         self.Chinese = Chinese
+        self.PinYin  = PinYin
         self.English = English
 
         self.LastUsed = -100
@@ -35,12 +36,23 @@ class Words:
         self.List.sort(key=myFunc)
         return self.List[0]
 
-terms = open("chinese.csv", "r", encoding="utf-8").readlines()
+
+p = Path(r'databases').glob('**/*')
+i = 1
+files = [file.name for file in list(p)]
+for file in files:
+    print(str(i) + ": " + file)
+    i = i +1
+CHOICE = int(input("Which file would you like to study: "))
+
+filename = "databases\\" + files[CHOICE - 1]
+
+terms = open(filename, "r", encoding="utf-8").readlines()
 ChineseWords = Words()
 for term in terms:
     term = term.replace("\n", "")
     row = term.split(",")
-    newWord = Word(row[0], row[1])
+    newWord = Word(row[0], row[1], row[2])
     ChineseWords.List.append(newWord)
 
 shuffle(ChineseWords.List)
@@ -49,12 +61,12 @@ round = 1
 while True:
     thing   = ChineseWords.getNewWord()
     answer  = input(thing.Chinese + ": ")
-    print(answer)
     if answer.lower() == thing.English.lower():
         thing.TimesCorrect += 1
+        print(thing.English + " (" + thing.PinYin + ")")
         print("Correct!")
     else:
-        print("Nope sorry the answer is: " + thing.English)
+        print("Nope sorry the answer is: " + thing.English + " (" + thing.PinYin + ")")
 
     thing.TimesAnswered += 1
     thing.updateAccuracy()
