6b3913cf3ff90450b586c57e1c3d774b687257fe
Samuel Low <s.low@share.epsb.ca>
2021-10-08 10:53:50 -0600

    Added more word sets!

--- main.py
+++ main.py
@@ -25,6 +25,12 @@ class Words:
     def __init__(self):
         self.List = []
 
+    def aveAccuracy(self):
+        sumAccuracy = 0
+        for word in self.List:
+            sumAccuracy = sumAccuracy + word.Accuracy
+        return sumAccuracy / len(self.List)
+
     def getNewWord(self):
         for word in self.List:
             gap = round - word.LastUsed
@@ -37,41 +43,42 @@ class Words:
         return self.List[0]
 
 
-p = Path(r'databases').glob('**/*')
-i = 1
-files = [file.name for file in list(p)]
-for file in files:
-    print(str(i) + ": " + file)
-    i = i +1
-CHOICE = int(input("Which file would you like to study: "))
-
-filename = "databases\\" + files[CHOICE - 1]
-
-terms = open(filename, "r", encoding="utf-8").readlines()
-ChineseWords = Words()
-for term in terms:
-    term = term.replace("\n", "")
-    row = term.split(",")
-    newWord = Word(row[0], row[1], row[2])
-    ChineseWords.List.append(newWord)
-
-shuffle(ChineseWords.List)
-round = 1
-
 while True:
-    thing   = ChineseWords.getNewWord()
-    answer  = input(thing.Chinese + ": ")
-    if answer.lower() == thing.English.lower():
-        thing.TimesCorrect += 1
+    p = Path(r'databases').glob('**/*')
+    i = 1
+    files = [file.name for file in list(p)]
+    for file in files:
+        print(str(i) + ": " + file.replace(".csv", ""))
+        i = i + 1
+    CHOICE = int(input("Which file would you like to study: "))
+
+    filename = "databases\\" + files[CHOICE - 1]
+
+    terms = open(filename, "r", encoding="utf-8").readlines()
+    ChineseWords = Words()
+    for term in terms:
+        term = term.replace("\n", "")
+        row = term.split(",")
+        newWord = Word(row[0], row[1], row[2])
+        ChineseWords.List.append(newWord)
+
+    shuffle(ChineseWords.List)
+    round = 1
+
+    while ChineseWords.aveAccuracy() < 0.95:
+        thing = ChineseWords.getNewWord()
+        answer = input(thing.Chinese + ": ")
         print(thing.English + " (" + thing.PinYin + ")")
-        print("Correct!")
-    else:
-        print("Nope sorry the answer is: " + thing.English + " (" + thing.PinYin + ")")
+        if answer.lower() == thing.English.lower():
+            thing.TimesCorrect += 1
+            print("Correct!")
+        else:
+            print("Nope sorry!")
 
-    thing.TimesAnswered += 1
-    thing.updateAccuracy()
-    thing.LastUsed = round
+        thing.TimesAnswered += 1
+        thing.updateAccuracy()
+        thing.LastUsed = round
 
-    round += 1
+        round += 1
 
-
+    print("Good job! You reached a 95% accuracy!")
