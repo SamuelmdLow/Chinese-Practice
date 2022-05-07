34982c8359863fcd9a1a8f51db6c268b5bb8db02
Samuel <samuel.m.d.low@gmail.com>
2022-05-07 13:37:19 -0600

    Fixed the path error

--- main.py
+++ main.py
@@ -42,52 +42,53 @@ class Words:
         self.List.sort(key=myFunc)
         return self.List[0]
 
+if __name__ == "__main__":
 
-while True:
-    p = Path(r'C:\Users\tubbd\PycharmProjects\Chinese-Practice\databases').glob('**/*')
-    i = 1
-    files = [file for file in p]
-    for file in files:
-        print(str(i) + ": " + file.name.replace(".csv", ""))
-        i = i + 1
-    CHOICE = int(input("Which file would you like to study: "))
+    while True:
+        p = Path("databases").glob('**/*')
+        i = 1
+        files = [file for file in p]
+        for file in files:
+            print(str(i) + ": " + file.name.replace(".csv", ""))
+            i = i + 1
+        CHOICE = int(input("Which file would you like to study: "))
 
-    # filename = "databases" + x + files[CHOICE - 1]
-    print("opening " + files[CHOICE - 1].name + "...")
-    filename = files[CHOICE - 1]
+        # filename = "databases" + x + files[CHOICE - 1]
+        print("opening " + files[CHOICE - 1].name + "...")
+        filename = files[CHOICE - 1]
 
-    terms = open(filename, "r", encoding="utf-8").readlines()
-    ChineseWords = Words()
-    for term in terms:
-        term = term.replace("\n", "")
-        row = term.split(",")
-        newWord = Word(row[0], row[1], row[2])
-        ChineseWords.List.append(newWord)
+        terms = open(filename, "r", encoding="utf-8").readlines()
+        ChineseWords = Words()
+        for term in terms:
+            term = term.replace("\n", "")
+            row = term.split(",")
+            newWord = Word(row[0], row[1], row[2])
+            ChineseWords.List.append(newWord)
 
-    shuffle(ChineseWords.List)
-    round = 1
+        shuffle(ChineseWords.List)
+        round = 1
 
-    isEnglish = input("Practice english meaning (1) or pinyin/character (2)?") == "1"
+        isEnglish = input("Practice english meaning (1) or pinyin/character (2)?") == "1"
 
-    while ChineseWords.aveAccuracy() < 0.95:
-        thing = ChineseWords.getNewWord()
-        answer = input(thing.Chinese + ": ")
-        print(thing.English + " (" + thing.PinYin + ")")
-        if isEnglish == True:
-            realAnswer = thing.English.lower()
-        else:
-            realAnswer = thing.Chinese.lower()
+        while ChineseWords.aveAccuracy() < 0.95:
+            thing = ChineseWords.getNewWord()
+            answer = input(thing.Chinese + ": ")
+            print(thing.English + " (" + thing.PinYin + ")")
+            if isEnglish == True:
+                realAnswer = thing.English.lower()
+            else:
+                realAnswer = thing.Chinese.lower()
 
-        if answer.lower() == realAnswer:
-            thing.TimesCorrect += 1
-            print("Correct! You are at " + str(ChineseWords.aveAccuracy() * 100) + "% accuracy!")
-        else:
-            print("Nope sorry!")
+            if answer.lower() == realAnswer:
+                thing.TimesCorrect += 1
+                print("Correct! You are at " + str(ChineseWords.aveAccuracy() * 100) + "% accuracy!")
+            else:
+                print("Nope sorry!")
 
-        thing.TimesAnswered += 1
-        thing.updateAccuracy()
-        thing.LastUsed = round
+            thing.TimesAnswered += 1
+            thing.updateAccuracy()
+            thing.LastUsed = round
 
-        round += 1
+            round += 1
 
-    print("Good job! You reached a 95% accuracy!")
+        print("Good job! You reached a 95% accuracy!")
